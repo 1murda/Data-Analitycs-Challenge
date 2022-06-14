c4e08eb7695685a8844dee426c0b28fcc7598dec
murda <gonzax102@gmail.com>
2022-06-14 05:11:56 -0300

    download data functionality

---
+++ src/get_data.py
@@ -0,0 +1,46 @@
+import requests
+import csv
+from utils import URLS
+import os
+from datetime import date
+
+# download date
+today: date = date.today()
+
+def get_data(url: str, category: str) -> None:
+    """ Get data from a given URL and write it to a csv file
+
+    Args:
+        url (str): url to get data from
+    """
+    # Get data from the url
+    try:
+            
+        download = requests.get(url)
+        
+        decoded_content: str = download.content.decode('utf-8')
+        
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        data = list(cr)
+
+        # create the category folder if it doesn't exist
+        dir = f"{category}/{str(today.year)}-{str(today.strftime('%B'))}"
+
+        if not os.path.exists(dir):
+                os.makedirs(dir)
+
+        # Write data to a csv file in the category folder
+        filename = f"{dir}/{category}-{str(today.day)}-{str(today.month)}-{str(today.year)}.csv"
+
+        with open(filename, "w") as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+                
+
+    except Exception as e:
+        print(f"Error al descargar {category}: {e}")
+
+
+if __name__ == "__main__":
+    for category, url in URLS.items():
+        get_data(url, category)
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+
+# Links de descarga de los archivos .CSV
+URLS = {
+    'museos': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv',
+    'salas-de-cine': 'https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae',
+    'bibliotecas-populares': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
+}
