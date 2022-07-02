5673c5654ef99bfa0dcfec3a5e68308e09328263
1murda <gonzalo[1;7rfi.uba.ar>
2022-07-02 05:03:48 -0300

    update 'salas de cine' link

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Links de descarga de los archivos .CSV
 URLS = {
     'museos': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv',
-    'salas-de-cine': 'https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_392ce1a8-ef11-4776-b280-6f1c7fae16ae',
+    'salas-de-cine': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv',
     'bibliotecas-populares': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
 }
